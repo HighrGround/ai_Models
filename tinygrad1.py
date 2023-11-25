1e0939dc17221d8de7e4cbf84ef06a237cbaa75d
DavidAtkins55 <caramelapplemac@gmail.com>
2023-11-25 21:39:27 +0000

    new changes to tinygrad

--- tinygrad1.py
+++ tinygrad1.py
@@ -3,6 +3,34 @@ import numpy as np
 import torch
 
 
+layer_outputs = [4.8,1.21, 2.385]
+e = 2.7182818
+
+
+#showcased in this video expontialization using eulers number nomralization to put all numbers to positive values while retaining information.
+#  on the whole this process can be defined as a softmax function.
+
+
+exp_values = np.exp(layer_outputs)
+
+for output in layer_outputs:
+    #by exponentiating the sum we can retain the information of the values while also converting the numbers to a positive value
+    exp_values.append(e**output) 
+
+#to do normalization
+
+norm_base  = sum(exp_values)
+norm_values = []
+
+for value in exp_values:
+    norm_values.append(value/norm_base)
+    print(norm_values)
+
+
+
+
+
+
 
 class Layer_dense:
     def __init__(self, n_inputs, n_neurons): 
@@ -13,8 +41,9 @@ class Layer_dense:
         self.z = np.dot(X, self.weigths) + self.biases
 
 
-class activation_ReLU:
+class activation_ReLU: 
     def forward(self, X):
         self.output = np.maximum(X, 0)
 
-    
+
+ 
