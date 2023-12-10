e78f10116b7ca81a9d28059d56282502f08053c5
DavidAtkins55 <caramelapplemac@gmail.com>
2023-12-10 11:48:34 +0000

    added databases/cooking with gas

--- makeshift2.py
+++ makeshift2.py
@@ -1,13 +1,56 @@
-import torch 
+import torch
+import numpy as np
+
+
+
+
+#model parameters
+input_size = 64
+
+
+#f ind avacado 
+#find what is macholibre 
+#implement loss function
+
+
+file_path = 'reales.csv'
+data = pd.read_csv(file_path)
+
+# Split the data into features (X) and target variable (y)
+X = data.drop('target_column_name', axis=1)  # Replace 'target_column_name' with the actual name of your target variable
+y = data['target_column_name']
+
+
+class DenseLayer:
+    def __init__(self, input_size, output_size, activation_function):
+        self.weights = np.random.randn(input_size, output_size)
+        self.bias = np.zeros((1, output_size))
+        self.activation_function = activation_function
+        self.input_data = None
+        self.output_data = None
+
+    def forward(self, input_data):
+        self.input_data = input_data
+        self.output_data = np.dot(input_data, self.weights) + self.bias
+        if self.activation_function is not None:
+            self.output_data = self.activation_function(self.output_data)
+        return self.output_data
+
+
+
+layer1 = DenseLayer(input_size)
+
+
+class loss_fucntion:
+    def __init__(self,):
+        
 
 
 
 
 
 
-
-
-
+'''
 class RNNCell(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -17,3 +60,5 @@ class RNNCell(nn.Module):
         xh = torch.cat([xt, hprev], dim=1)
         ht = F.tanh(self.xh_to_h(xh))
         return ht
+
+'''
